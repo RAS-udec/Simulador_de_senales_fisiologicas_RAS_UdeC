85d777f5554e7683b4d2696f4dec2c92bd46e73f
Gquilodran <gquilodran2024@udec.cl>
2025-11-12 13:36:52 -0300

    Refactory: Se actualizaron ciertas funciones como Pulso e se leds
    Updated the Pulso function to handle long integers and modified the beep function to include a ppm parameter for improved functionality.

--- Codigo Completo/Simulador/funciones.ino
+++ Codigo Completo/Simulador/funciones.ino
@@ -7,29 +7,56 @@ para facilitar la lectura del código.
 La función pulso transforma valores entre 0 y 1023 a valores que se asemejan al pulso, 
 entre 0 y FCM.
 */
-int Pulso(int valor, int fcm){
-  int ppm = (valor * fcm)/1023;
+long Pulso(long valor, long fcm){
+  long ppm = ((valor) * (fcm))/1023;
   return ppm;
 }
 
-void beep(int caso){
-  if(caso==1){
-    tone(buzzer,1000);
-  }
-  else if(caso == 2){
-    tone(buzzer, 1500, 250);
-    delay(200);
-    tone(buzzer, 1500, 250);
-    delay(200);
-  }
-  else if(caso == 3){
+void beep(int caso, int ppm) {
+  if (ppm < 30) ppm = 30;  // evita división por 0 o pausas enormes
+  int pausa;
 
-  }
-  else if(caso == 4){
+  switch (caso) {
+    case 1: // Línea plana
+      tone(BUZZER, 1000);
+      break;
 
-  }
-  else if(caso == 5){
+    case 2: // Rango normal (80-100 aprox)
+      pausa = 60000 / ppm;  // 60.000 ms por minuto
+      tone(BUZZER, 800, 100);
+      delay(pausa+500);
+      break;
 
+    case 3: // Bajo ritmo
+      pausa = 1000;
+      tone(BUZZER, 800, 100);
+      delay(pausa);
+      break;
+
+    case 4: // Taquicardia o máxima
+      pausa = 6000 / ppm;   // pitidos más rápidos
+      tone(BUZZER, 800, 100);
+      delay(pausa);
+      break;
   }
 }
 
+void leds(int caso){
+  switch(caso){
+    case 1: //La LED roja se enciende y la verde se apaga
+      digitalWrite(LED_ROJO, HIGH);
+      digitalWrite(LED_VERDE, LOW);
+      break;
+    case 2: //La LED verde se apaga mientras que la roja parpadea en intervalos
+      digitalWrite(LED_VERDE, LOW);
+      digitalWrite(LED_ROJO, HIGH);
+      delay(500);
+      digitalWrite(LED_ROJO, LOW);
+      delay(200);
+      break;
+    case 3: //La LED verde se enciende y la roja se apaga
+      digitalWrite(LED_ROJO, LOW);
+      digitalWrite(LED_VERDE, HIGH);
+      break;
+  }
+}
